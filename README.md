e8e235723389ad489a57807467d35c41d6ae9891
lortegasanpedro <luisangelortegasanpedro@gmail.com>
2024-03-15 18:51:00 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ README.md
@@ -0,0 +1,24 @@
+
+> Abra esta pagina en [https://lortegasanpedro.github.io/1-maqueen-plus-v2/](https://lortegasanpedro.github.io/1-maqueen-plus-v2/)
+
+## Usar como extensión
+
+Este repositorio puede ser añadido como una **extensión** en MakeCode.
+
+* abra [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* haga clic en **New Project**
+* haga clic en **Extensiones** en el menú del engranaje
+* buscar **https://github.com/lortegasanpedro/1-maqueen-plus-v2** e importar
+
+## Edita este proyecto
+
+Para editar este repositorio en MakeCode.
+
+* abra [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* haga clic en **Import** luego haga clic en **Import URL**
+* pegue **https://github.com/lortegasanpedro/1-maqueen-plus-v2** y haga clic en importar
+
+#### Metadatos (utilizados para búsqueda, renderizado)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,62 @@
+let direction = 0
+let distance = 0
+let n = 0
+let R = 0
+let VELOCIDAD = 35
+DFRobotMaqueenPlusV2.init()
+DFRobotMaqueenPlusV2.controlLED(MyEnumLed.eAllLed, MyEnumSwitch.eOpen)
+DFRobotMaqueenPlusV2.setBrightness(100)
+let G = 1
+let B = 2
+let P = 3
+basic.forever(function () {
+    if (R <= 3) {
+        DFRobotMaqueenPlusV2.setIndexColor(R, 0xff0000)
+        R += 1
+    } else {
+        R = 0
+    }
+    if (G <= 3) {
+        DFRobotMaqueenPlusV2.setIndexColor(G, 0x00ff00)
+        G += 1
+    } else {
+        G = 0
+    }
+    if (B <= 3) {
+        DFRobotMaqueenPlusV2.setIndexColor(B, 0x007fff)
+        B += 1
+    } else {
+        B = 0
+    }
+    if (P <= 3) {
+        DFRobotMaqueenPlusV2.setIndexColor(P, 0xff00ff)
+        P += 1
+    } else {
+        P = 0
+    }
+    n += 1
+    basic.pause(1000)
+})
+basic.forever(function () {
+    distance = DFRobotMaqueenPlusV2.readUltrasonic(DigitalPin.P13, DigitalPin.P14)
+    // basic.showNumber(distance)
+    direction = randint(1, 2)
+    // basic.showNumber(direction)
+    if (distance < 30) {
+        if (direction == 1) {
+            DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eLeftMotor, MyEnumDir.eForward, VELOCIDAD * 2)
+            DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eRightMotor, MyEnumDir.eForward, 0)
+            basic.pause(1000)
+        }
+        if (direction == 2) {
+            DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eLeftMotor, MyEnumDir.eForward, 0)
+            DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eRightMotor, MyEnumDir.eForward, VELOCIDAD * 2)
+            basic.pause(1000)
+        }
+    } else {
+        DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eAllMotor, MyEnumDir.eForward, VELOCIDAD)
+    }
+})
+basic.forever(function () {
+    basic.showNumber(distance)
+})
