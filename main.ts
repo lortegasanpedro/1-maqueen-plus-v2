d1be98608efccaa1e3309554fa72ca2a81fef20f
lortegasanpedro <luisangelortegasanpedro@gmail.com>
2024-03-19 23:29:10 +0100

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,40 @@
+function direccion () {
+    direction = randint(1, 2)
+    if (direction == 1) {
+        direction1()
+    }
+    if (direction == 2) {
+        direction2()
+    }
+}
+function bacward () {
+    datalogger.log(datalogger.createCV("D3", distance))
+    DFRobotMaqueenPlusV2.controlLED(MyEnumLed.eLeftLed, MyEnumSwitch.eOpen)
+    DFRobotMaqueenPlusV2.controlLED(MyEnumLed.eRightLed, MyEnumSwitch.eOpen)
+    DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eLeftMotor, MyEnumDir.eBackward, VELOCIDAD)
+    DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eRightMotor, MyEnumDir.eBackward, VELOCIDAD)
+    basic.pause(1000)
+    DFRobotMaqueenPlusV2.controlMotorStop(MyEnumMotor.eAllMotor)
+    direccion()
+}
+function direction2 () {
+    datalogger.log(datalogger.createCV("D2", distance))
+    DFRobotMaqueenPlusV2.controlLED(MyEnumLed.eLeftLed, MyEnumSwitch.eClose)
+    DFRobotMaqueenPlusV2.controlLED(MyEnumLed.eRightLed, MyEnumSwitch.eOpen)
+    DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eLeftMotor, MyEnumDir.eForward, 0)
+    DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eRightMotor, MyEnumDir.eForward, VELOCIDAD * 2)
+    basic.pause(1000)
+    DFRobotMaqueenPlusV2.controlMotorStop(MyEnumMotor.eAllMotor)
+}
+function direction1 () {
+    datalogger.log(datalogger.createCV("D1", distance))
+    DFRobotMaqueenPlusV2.controlLED(MyEnumLed.eLeftLed, MyEnumSwitch.eOpen)
+    DFRobotMaqueenPlusV2.controlLED(MyEnumLed.eRightLed, MyEnumSwitch.eClose)
+    DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eLeftMotor, MyEnumDir.eForward, VELOCIDAD * 2)
+    DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eRightMotor, MyEnumDir.eForward, 0)
+    basic.pause(1000)
+    DFRobotMaqueenPlusV2.controlMotorStop(MyEnumMotor.eAllMotor)
+}
 /**
  * basic.forever(function () {
  * 
@@ -11,6 +48,24 @@
 /**
  * Leds
  */
+let distance = 0
+let direction = 0
+let VELOCIDAD = 0
+let R = 0
+let n = 0
+VELOCIDAD = 60
+let G = 1
+let B = 2
+let P = 3
+DFRobotMaqueenPlusV2.init()
+DFRobotMaqueenPlusV2.controlLED(MyEnumLed.eAllLed, MyEnumSwitch.eClose)
+DFRobotMaqueenPlusV2.setBrightness(100)
+// Logica del motor
+datalogger.setColumnTitles(
+"D1",
+"D2",
+"D3"
+)
 /**
  * basic.forever(function () {
  * 
@@ -68,62 +123,6 @@
  * 
  * })
  */
-
-function direccion() {
-    direction = randint(1, 2)
-    if (direction == 1) {
-        direction1()
-    }
-    if (direction == 2) {
-        direction2()
-    }
-}
-function bacward() {
-    datalogger.log(datalogger.createCV("D3", distance))
-    DFRobotMaqueenPlusV2.controlLED(MyEnumLed.eLeftLed, MyEnumSwitch.eOpen)
-    DFRobotMaqueenPlusV2.controlLED(MyEnumLed.eRightLed, MyEnumSwitch.eOpen)
-    DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eLeftMotor, MyEnumDir.eBackward, VELOCIDAD)
-    DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eRightMotor, MyEnumDir.eBackward, VELOCIDAD)
-    basic.pause(1000)
-    DFRobotMaqueenPlusV2.controlMotorStop(MyEnumMotor.eAllMotor)
-    direccion()
-}
-function direction2() {
-    datalogger.log(datalogger.createCV("D2", distance))
-    DFRobotMaqueenPlusV2.controlLED(MyEnumLed.eLeftLed, MyEnumSwitch.eClose)
-    DFRobotMaqueenPlusV2.controlLED(MyEnumLed.eRightLed, MyEnumSwitch.eOpen)
-    DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eLeftMotor, MyEnumDir.eForward, 0)
-    DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eRightMotor, MyEnumDir.eForward, VELOCIDAD * 2)
-    basic.pause(1000)
-    DFRobotMaqueenPlusV2.controlMotorStop(MyEnumMotor.eAllMotor)
-}
-function direction1() {
-    datalogger.log(datalogger.createCV("D1", distance))
-    DFRobotMaqueenPlusV2.controlLED(MyEnumLed.eLeftLed, MyEnumSwitch.eOpen)
-    DFRobotMaqueenPlusV2.controlLED(MyEnumLed.eRightLed, MyEnumSwitch.eClose)
-    DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eLeftMotor, MyEnumDir.eForward, VELOCIDAD * 2)
-    DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eRightMotor, MyEnumDir.eForward, 0)
-    basic.pause(1000)
-    DFRobotMaqueenPlusV2.controlMotorStop(MyEnumMotor.eAllMotor)
-}
-let direction = 0
-let distance = 0
-let VELOCIDAD = 0
-let n = 0
-let R = 0
-VELOCIDAD = 60
-let G = 1
-let B = 2
-let P = 3
-DFRobotMaqueenPlusV2.init()
-DFRobotMaqueenPlusV2.controlLED(MyEnumLed.eAllLed, MyEnumSwitch.eClose)
-DFRobotMaqueenPlusV2.setBrightness(100)
-// Logica del motor
-datalogger.setColumnTitles(
-"D1",
-"D2",
-"D3"
-)
 basic.forever(function () {
     distance = DFRobotMaqueenPlusV2.readUltrasonic(DigitalPin.P13, DigitalPin.P14)
     // basic.showNumber(distance)
