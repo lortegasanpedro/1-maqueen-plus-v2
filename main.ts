0efa2b890dd6680709f731deffc4b4a36cb2293b
lortegasanpedro <luisangelortegasanpedro@gmail.com>
2024-03-30 17:04:35 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
-
+// serial.writeLine("direccion end")
 function direccion () {
-//    serial.writeLine("direccion init")
+    // serial.writeLine("direccion init")
     direction = randint(1, 2)
     if (direction == 1) {
         directionLeftMotor()
@@ -8,69 +8,36 @@ function direccion () {
     if (direction == 2) {
         directionRightMotor()
     }
- //   serial.writeLine("direccion end")
+}
+// serial.writeLine("directionRightMotor End")
+function directionRightMotor () {
+    // serial.writeLine("directionRightMotor Init")
+    // datalogger.log(datalogger.createCV("D2", distance))
+    DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eLeftMotor, MyEnumDir.eForward, 0)
+    DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eRightMotor, MyEnumDir.eForward, velocidad * 2)
+    basic.pause(pauseGiro)
+    DFRobotMaqueenPlusV2.controlMotorStop(MyEnumMotor.eAllMotor)
+}
+// serial.writeLine("directionLeftMotor End")
+function directionLeftMotor () {
+    // datalogger.log(datalogger.createCV("D1", distance))
+    // serial.writeLine("directionLeftMotor Init")
+    DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eLeftMotor, MyEnumDir.eForward, velocidad * 2)
+    DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eRightMotor, MyEnumDir.eForward, 0)
+    basic.pause(pauseGiro)
+    DFRobotMaqueenPlusV2.controlMotorStop(MyEnumMotor.eAllMotor)
 }
 // serial.writeLine("backward END")
+// serial.writeLine("bacward End")
 function backward () {
-//    serial.writeLine("bacward Init")
+    // serial.writeLine("bacward Init")
     // datalogger.log(datalogger.createCV("D3", distance))
     DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eLeftMotor, MyEnumDir.eBackward, velocidad)
     DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eRightMotor, MyEnumDir.eBackward, velocidad)
     basic.pause(1000)
     DFRobotMaqueenPlusV2.controlMotorStop(MyEnumMotor.eAllMotor)
     direccion()
-//    serial.writeLine("bacward End")
 }
-function directionRightMotor () {
-//    serial.writeLine("directionRightMotor Init")
-    // datalogger.log(datalogger.createCV("D2", distance))
-    DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eLeftMotor, MyEnumDir.eForward, 0)
-    DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eRightMotor, MyEnumDir.eForward, velocidad * 2)
-    basic.pause(pauseGiro)
-    DFRobotMaqueenPlusV2.controlMotorStop(MyEnumMotor.eAllMotor)
-//    serial.writeLine("directionRightMotor End")
-}
-function directionLeftMotor () {
-    // datalogger.log(datalogger.createCV("D1", distance))
-//    serial.writeLine("directionLeftMotor Init")
-    DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eLeftMotor, MyEnumDir.eForward, velocidad * 2)
-    DFRobotMaqueenPlusV2.controlMotor(MyEnumMotor.eRightMotor, MyEnumDir.eForward, 0)
-    basic.pause(pauseGiro)
-    DFRobotMaqueenPlusV2.controlMotorStop(MyEnumMotor.eAllMotor)
-//    serial.writeLine("directionLeftMotor End")
-}
-/*
-input.onSound(DetectedSound.Loud, function () {
-    serial.writeLine("DetectedSound.Loud")
-    aplauso = !aplauso
-    if (aplauso) {
-        DFRobotMaqueenPlusV2.setIndexColor(3, 0xff0000)
-    } else {
-        DFRobotMaqueenPlusV2.setIndexColor(3, 0x0000ff)
-    }
-})*/
-
-let roll = 0
-let distance = 0
-let direction = 0
-let aplauso = true
-let pauseGiro = 0
-let velocidad = 0
-let x = 0
-let y = 0
-let z = 0
-let speed = 0
-velocidad = 50
-pauseGiro = 500
-// let n = 0
-// let R = 0
-// let G = 1
-// let B = 2
-// let P = 3
-DFRobotMaqueenPlusV2.init()
-DFRobotMaqueenPlusV2.controlLED(MyEnumLed.eAllLed, MyEnumSwitch.eClose)
-DFRobotMaqueenPlusV2.setBrightness(100)
-//input.setSoundThreshold(SoundThreshold.Loud, 60)
 /**
  * Logica del motor
  */
@@ -88,6 +55,48 @@ DFRobotMaqueenPlusV2.setBrightness(100)
  * "speed"
  * 
  * )
+ */
+let roll = 0
+let distance = 0
+let direction = 0
+let pauseGiro = 0
+let velocidad = 0
+let speed = 0
+let z = 0
+let y = 0
+let x = 0
+let aplauso = true
+velocidad = 50
+pauseGiro = 500
+// let n = 0
+// let R = 0
+// let G = 1
+// let B = 2
+// let P = 3
+DFRobotMaqueenPlusV2.init()
+DFRobotMaqueenPlusV2.controlLED(MyEnumLed.eAllLed, MyEnumSwitch.eClose)
+DFRobotMaqueenPlusV2.setBrightness(100)
+/**
+ * input.setSoundThreshold(SoundThreshold.Loud, 60)
+ */
+/**
+ * input.onSound(DetectedSound.Loud, function () {
+ * 
+ * serial.writeLine("DetectedSound.Loud")
+ * 
+ * aplauso = !aplauso
+ * 
+ * if (aplauso) {
+ * 
+ * DFRobotMaqueenPlusV2.setIndexColor(3, 0xff0000)
+ * 
+ * } else {
+ * 
+ * DFRobotMaqueenPlusV2.setIndexColor(3, 0x0000ff)
+ * 
+ * }
+ * 
+ * })
  */
 // basic.forever(function () {
 // 
@@ -145,9 +154,9 @@ DFRobotMaqueenPlusV2.setBrightness(100)
 // 
 // })
 basic.forever(function () {
-    //serial.writeLine("Robot forever Init : ")
-    if (input.soundLevel() > 128) {
-        aplauso = !aplauso
+    // serial.writeLine("Robot forever Init : ")
+    if (input.soundLevel() > 168) {
+        aplauso = !(aplauso)
         if (aplauso) {
             DFRobotMaqueenPlusV2.setIndexColor(3, 0xff0000)
         } else {
@@ -185,6 +194,6 @@ basic.forever(function () {
     } else {
         DFRobotMaqueenPlusV2.controlMotorStop(MyEnumMotor.eAllMotor)
     }
-    //serial.writeLine("Robot forever End : ")
+    // serial.writeLine("Robot forever End : ")
     basic.pause(30)
 })
